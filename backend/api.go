06c0c3881ba65e0770f122e825be2054233968f4
IainHenn <Iainevans2005@gmail.com>
2025-07-12 09:27:19 -0700

    Feature: Basic login

--- backend/api.go
+++ backend/api.go
@@ -1,21 +1,72 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
 )
 
-func getHi(c *gin.Context) {
-	fmt.Println("Hi")
+func loginUser(c *gin.Context) {
+
+	userEmail := c.Query("email")
+	userPassword := c.Query("password")
+
+	// Database access
+	dbUser := os.Getenv("DB_USER")
+	dbPW := os.Getenv("DB_PW")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	connectionStr := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", dbUser, dbPW, dbHost, dbName)
+	db, err := sql.Open("postgres", connectionStr)
+
+	if err != nil {
+		fmt.Println("Error opening database:", err)
+		c.Status(500)
+		return
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("Error connecting to database:", err)
+		c.Status(500)
+		return
+	}
+
+	// Definition of a user
+	type user struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	// Assigning user struct
+	u := user{
+		userEmail,
+		userPassword,
+	}
+
+	//Checking if the user exists, if they don't return
+	row := db.QueryRow("SELECT email, password FROM users WHERE email = ? and password = ?", u.Email, u.Password)
+	err = row.Scan(&u.Email, &u.Password)
+
+	if err != nil {
+		fmt.Println("Error finding user!")
+		c.Status(404)
+	}
+
+	// If it reaches here then it was a success
+	c.Status(200)
 }
 
 func main() {
 	fmt.Println("Starting server on port 8080...")
 	router := gin.Default()
 	router.Use(cors.Default())
-	router.GET("/hi", getHi)
-	router.Run("0.0.0.0:8080")
+	router.POST("/sessions", loginUser)
+	router.Run("localhost:8080")
 	fmt.Println("Running!")
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,103 +1,71 @@
+"use client"
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const signIn = (event: React.FormEvent) => {
+    console.log("in here");
+    event.preventDefault();
+    fetch("http://localhost:8080/sessions", {
+      method: "POST",
+      body: JSON.stringify({ email, password })
+    })
+      .then(response => {
+        if(response.status == 200){
+          console.log("User exists");
+        } else {
+          console.log(`Failed to login user: ${response.status}`)
+        }
+      })
+      .catch(error => {
+      // handle error
+      console.error("Error:", error);
+      });
+  }
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-purple-950">
+      <div className="bg-blue-500 rounded-2xl shadow-lg p-8 w-[30%] h-75 text-black">
+          <form className="flex flex-col space-y-6" onClick={signIn}>
+            <div className="flex flex-col space-y-2">
+              <label htmlFor="email" className="text-white font-semibold">
+              Email
+              </label>
+              <input
+              id="email"
+              type="email"
+              className="bg-gray-100 text-black placeholder-gray-500 hover:bg-gray-600 hover:placeholder-black rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+              placeholder="Enter your email"
+              autoComplete="email"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col space-y-2">
+              <label htmlFor="password" className="text-white font-semibold">
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                className="bg-gray-100 text-black placeholder-gray-500 hover:bg-gray-600 hover:placeholder-black rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+                placeholder="Enter your password"
+                autoComplete="current-password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-purple-700 hover:bg-purple-800 text-white font-bold py-2 rounded-md transition"
+            >
+              Sign In
+            </button>
+          </form>
+      </div>
     </div>
   );
 }
